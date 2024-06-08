5ad6fd08659b661f9c33f14385df8adbf903dda6
kudononoka <106291674+kudononoka@users.noreply.github.com>
2024-06-08 12:17:44 +0900

    [update]ボール生成とシーンの流れ

--- PS22B_KudoNonoka_2024T1/Main.cpp
+++ PS22B_KudoNonoka_2024T1/Main.cpp
@@ -1,20 +1,34 @@
 ﻿# include <Siv3D.hpp>
-
+# include <vector>
 /*
-	古き良き書き方での実装
-	・安全性や利便性などは一切考えていない
+	よりC++ライクな書き方
+	・クラスベース
+	・継承を行う
 */
 
-namespace constants{
+//==============================
+// 前方宣言
+//==============================
+class Ball;
+class Bricks;
+class Paddle;
 
-	namespace ball {
-		/// @brief ボールの速さ
-		constexpr double BALL_SPEED = 480.0;
-	}
 
-	namespace bricks {
+enum SceneState
+{
+	TITLE,
+	IN_GAME,
+	GAMEOVER,
+	CLEAR
+};
+
+//==============================
+// 定数
+//==============================
+namespace constants {
+	namespace brick {
 		/// @brief ブロックのサイズ
-		constexpr Size BRICK_SIZE{ 40, 20 };
+		constexpr Size SIZE{ 40, 20 };
 
 		/// @brief ブロックの数　縦
 		constexpr int Y_COUNT = 5;
@@ -26,144 +40,364 @@ namespace constants{
 		constexpr int MAX = Y_COUNT * X_COUNT;
 	}
 
+	namespace ball {
+		/// @brief ボールの速さ
+		constexpr double SPEED = 480.0;
+	}
+
 	namespace paddle {
-		constexpr Size SIZE{ 40, 20 };
+		/// @brief パドルのサイズ
+		constexpr Size SIZE{ 100, 10 };
+	}
+
+	namespace reflect {
+		/// @brief 縦方向ベクトル
+		constexpr Vec2 VERTICAL{ 1, -1 };
+
+		/// @brief 横方向ベクトル
+		constexpr Vec2 HORIZONTAL{ -1,  1 };
 	}
 }
 
-class Ball {
-public:
+
+
+//==============================
+// クラス宣言
+//==============================
+/// @brief ボール
+class Ball final {
+private:
+	/// @brief 速度
 	Vec2 velocity;
-	Circle model;
 
-	Ball() :
-		velocity(0, -constants::ball::BALL_SPEED),
-		model{400,400,10} {}
+	/// @brief ボール
+	Circle ball;
 
+public:
+	/// @brief コンストラクタ
+	Ball() : velocity({ 0, -constants::ball::SPEED }), ball({ 400, 400, 8 }) {}
+
+	/// @brief デストラクタ
+	~Ball() {}
+
+	/// @brief 更新
 	void Update() {
-		model.moveBy(velocity * Scene::DeltaTime());
+		ball.moveBy(velocity * Scene::DeltaTime());
 	}
 
-	void Draw() {
-		model.draw();
+	/// @brief 描画
+	void Draw() const {
+		ball.draw();
+	}
+
+	Circle GetCircle() const {
+
+		return ball;
+	}
+
+	Vec2 GetVelocity() const {
+		return velocity;
+	}
+
+	/// @brief 新しい移動速度を設定
+	/// @param newVelocity 新しい移動速度
+	void SetVelocity(Vec2 newVelocity) {
+		using namespace constants::ball;
+		velocity = newVelocity.setLength(SPEED);
+	}
+
+	/// @brief 反射
+	/// @param reflectVec 反射ベクトル方向 
+	void Reflect(const Vec2 reflectVec) {
+		velocity *= reflectVec;
 	}
 };
 
-class Bricks {
+class BallSpawner {
+private:
+	//std::pmr::vector<Ball*> balls;
+	Ball* ballArray[5];
+	int ballArrayLenght = 5;
+	int index = 0;
 public:
-	Rect bricksModel[constants::bricks::MAX];
+	void Generate() {
+		//balls.push_back(new Ball());
+		if (index == ballArrayLenght) return;
+		ballArray[index] = new Ball();
+		index++;
+	}
 
-	Bricks() {
-		using namespace constants::bricks;
+	void Delete() {
+		for (int i = 0; i < index; i++) {
+			if (ballArray[i] != nullptr)
+			{
+				delete ballArray[i];
+				ballArray[i] = nullptr;
+			}
+		}
+		index = 0;
+	}
+
+	Ball* GetBalls(int index) {
+		return ballArray[index];
+	}
+
+	int GetBallCount(){
+		return index;
+	}
+};
+
+/// @brief ブロック
+class Bricks final {
+private:
+	/// @brief ブロックリスト
+	Rect brickTable[constants::brick::MAX];
+
+public:
+
+	/// @brief デストラクタ
+	~Bricks() {}
+
+	/// @brief 衝突検知
+	void Intersects(Ball* const target);
+
+	/// @brief 描画
+	void Draw() const {
+		using namespace constants::brick;
+
+		for (int i = 0; i < MAX; ++i) {
+			brickTable[i].stretched(-1).draw(HSV{ brickTable[i].y - 40 });
+		}
+	}
+
+	void Instance()
+	{
+		using namespace constants::brick;
 		for (int y = 0; y < Y_COUNT; ++y) {
 			for (int x = 0; x < X_COUNT; ++x) {
 				int index = y * X_COUNT + x;
-				bricksModel[index] = Rect{
-					x * BRICK_SIZE.x,
-					60 + y * BRICK_SIZE.y,
-					BRICK_SIZE
+				brickTable[index] = Rect{
+					x * SIZE.x,
+					60 + y * SIZE.y,
+					SIZE
 				};
 			}
 		}
 	}
+};
 
-	void Draw() {
-		for (int i = 0; i < constants::bricks::MAX; ++i) {
-			bricksModel[i].stretched(-1).draw(HSV{ bricksModel[i].y - 40 });
-	    }
+/// @brief パドル
+class Paddle final {
+private:
+	Rect paddle;
+
+public:
+	/// @brief コンストラクタ
+	Paddle() : paddle(Rect(Arg::center(Cursor::Pos().x, 500), constants::paddle::SIZE)) {}
+
+	/// @brief デストラクタ
+	~Paddle() {}
+
+	/// @brief 衝突検知
+	void Intersects(Ball* const target) const;
+
+	/// @brief 更新
+	void Update() {
+		paddle.x = Cursor::Pos().x - (constants::paddle::SIZE.x / 2);
 	}
 
-	void Intersects(Ball& ball) {
-		for (int i = 0; i < constants::bricks::MAX; ++i) {
-			// 参照で保持
-			Rect& refBrick = bricksModel[i];
+	/// @brief 描画
+	void Draw() const {
+		paddle.rounded(3).draw();
+	}
+};
 
-			// 衝突を検知
-			if (refBrick.intersects(ball.model))
-			{
-				// ブロックの上辺、または底辺と交差
-				if (refBrick.bottom().intersects(ball.model) || refBrick.top().intersects(ball.model))
-				{
-					ball.velocity.y *= -1;
-				}
-				else // ブロックの左辺または右辺と交差
-				{
-					ball.velocity.x *= -1;
-				}
+/// @brief 壁
+class Wall {
+public:
+	/// @brief 衝突検知
+	static void Intersects(Ball* target) {
+		using namespace constants;
 
-				// あたったブロックは画面外に出す
-				refBrick.y -= 600;
+		if (!target) {
+			return;
+		}
 
-				// 同一フレームでは複数のブロック衝突を検知しない
-				break;
-			}
+		auto velocity = target->GetVelocity();
+		auto ball = target->GetCircle();
+
+		// 天井との衝突を検知
+		if ((ball.y < 0) && (velocity.y < 0))
+		{
+			target->Reflect(reflect::VERTICAL);
+		}
+
+		// 壁との衝突を検知
+		if (((ball.x < 0) && (velocity.x < 0))
+			|| ((Scene::Width() < ball.x) && (0 < velocity.x)))
+		{
+			target->Reflect(reflect::HORIZONTAL);
 		}
 	}
 };
 
-class Paddle {
+class GameManager
+{
+private:
+	/// @brief 現在のシーン
+	SceneState _currentScene;
 public:
-	Rect model;
-
-	Paddle() :
-		model(Arg::center(Cursor::Pos().x, 500), 60, 10)
-	{}
-
-	void Update() {
-		model.x = Cursor::Pos().x - (constants::paddle::SIZE.x / 2);
+	/// @brief シーン遷移
+	void SceneChange(SceneState state)
+	{
+		_currentScene = state;
 	}
 
-	void Intersects(Ball& ball) {
-		if ((0 < ball.velocity.y) && model.intersects(ball.model))
-        {
-			ball.velocity = Vec2{
-		    (ball.model.x - model.center().x) * 10,
-		    -ball.velocity.y
-	        }.setLength(constants::ball::BALL_SPEED);
-        }
-	}
-
-	void Draw() {
-		model.rounded(3).draw();
+	SceneState GetCurrentScene()
+	{
+		return _currentScene;
 	}
 };
 
+//==============================
+// 定義
+//==============================
+void Bricks::Intersects(Ball* const target) {
+	using namespace constants;
+	using namespace constants::brick;
+
+	if (!target) {
+		return;
+	}
+
+	auto ball = target->GetCircle();
+
+	for (int i = 0; i < MAX; ++i) {
+		// 参照で保持
+		Rect& refBrick = brickTable[i];
+
+		// 衝突を検知
+		if (refBrick.intersects(ball))
+		{
+			// ブロックの上辺、または底辺と交差
+			if (refBrick.bottom().intersects(ball)
+				|| refBrick.top().intersects(ball))
+			{
+				target->Reflect(reflect::VERTICAL);
+			}
+			else // ブロックの左辺または右辺と交差
+			{
+				target->Reflect(reflect::HORIZONTAL);
+			}
+
+			// あたったブロックは画面外に出す
+			refBrick.y -= 600;
+
+			// 同一フレームでは複数のブロック衝突を検知しない
+			break;
+		}
+	}
+}
+
+void Paddle::Intersects(Ball* const target) const {
+	if (!target) {
+		return;
+	}
+
+	auto velocity = target->GetVelocity();
+	auto ball = target->GetCircle();
+
+	if ((0 < velocity.y) && paddle.intersects(ball))
+	{
+		target->SetVelocity(Vec2{
+			(ball.x - paddle.center().x) * 10,
+			-velocity.y
+		});
+	}
+}
+
+//==============================
+// エントリー
+//==============================
 void Main()
 {
-	Ball ball;
 	Bricks bricks;
+	//Ball ball;
 	Paddle paddle;
+	BallSpawner ballSpawner;
+
+	GameManager gameManager;
+	gameManager.SceneChange(SceneState::TITLE);
+
+	const Font font{ FontMethod::MSDF, 60};
+	int32 count = 0;
 
 	while (System::Update())
 	{
-		//更新
-		ball.Update();
-		paddle.Update();
+		switch (gameManager.GetCurrentScene())
+		{
+			//タイトルシーン
+			case SceneState::TITLE:
+				font(U"Breaking blocks"_fmt(count)).drawAt(Scene::Center(),ColorF{ 0.2, 0.6, 0.9 });
+				if (SimpleGUI::Button(U"START", Vec2{ Scene::Center().x - 50, Scene::Center().y + 80 }, 100))
+				{
+					gameManager.SceneChange(SceneState::IN_GAME);
+					//ブロックの初期化
+					bricks.Instance();
+					//ボール生成
+					ballSpawner.Generate();
+				}
+				break;
 
-		//当たり判定
-		bricks.Intersects(ball);
-		paddle.Intersects(ball);
+			//ゲームシーン
+			case SceneState::IN_GAME:
+				//==============================
+				// 更新
+				//==============================
+				paddle.Update();
+				for (int i = 0; i < ballSpawner.GetBallCount(); i++) {
+					ballSpawner.GetBalls(i)->Update();
+				}
 
-		//描画
-		ball.Draw();
-		bricks.Draw();
-		paddle.Draw();
+				//==============================
+				// コリジョン
+				//==============================
+				for (int i = 0; i < ballSpawner.GetBallCount(); i++) {
+					Ball* ball = ballSpawner.GetBalls(i);
+					bricks.Intersects(ball);
+					Wall::Intersects(ball);
+					paddle.Intersects(ball);
+				}
 
-		////==============================
-		//// コリジョン
-		////==============================
 
-		//// 天井との衝突を検知
-		//if ((ballModel.y < 0) && (ballVelocity.y < 0))
-		//{
-		//	ballVelocity.y *= -1;
-		//}
+				//==============================
+				// 描画
+				//==============================
+				bricks.Draw();
+				paddle.Draw();
+				for (int i = 0; i < ballSpawner.GetBallCount(); i++) {
+					ballSpawner.GetBalls(i)->Draw();
+				}
 
-		//// 壁との衝突を検知
-		//if (((ballModel.x < 0) && (ballVelocity.x < 0))
-		//	|| ((Scene::Width() < ballModel.x) && (0 < ballVelocity.x)))
-		//{
-		//	ballVelocity.x *= -1;
-		//}
+				//ボール生成
+				if (MouseR.down()) {
+					ballSpawner.Generate();
+				}
+
+				//ゲームオーバー判定
+				for (int i = 0; i < ballSpawner.GetBallCount(); i++) {
+					Circle ball = ballSpawner.GetBalls(i)->GetCircle();
+					if (ball.y > 800)
+					{
+						//ボールメモリ解放
+						ballSpawner.Delete();
+						//タイトルに戻る
+						gameManager.SceneChange(SceneState::TITLE);
+					}
+				}
+				break;
+		}
 	}
+	ballSpawner.Delete();
 }
+
